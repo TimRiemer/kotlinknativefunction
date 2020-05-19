3bc99da7b3aace2cc40d03851de69ade555d2142
Tim Riemer <tim.riemer@gmx.de>
2020-05-19 06:57:30 +0200

    Update to Hoxton.SR4 Add org.json:json to fulfill missing dependencies for Hoxton.SR4

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,7 +14,7 @@ repositories {
 	mavenCentral()
 }
 
-extra["springCloudVersion"] = "Hoxton.SR3"
+extra["springCloudVersion"] = "Hoxton.SR4"
 
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter")
@@ -22,6 +22,7 @@ dependencies {
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 	implementation("org.springframework.cloud:spring-cloud-starter-function-web")
 	implementation("org.springframework.cloud:spring-cloud-function-kotlin")
+	implementation("org.json:json:20190722")
 	testImplementation("org.springframework.boot:spring-boot-starter-test") {
 		exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
 	}
